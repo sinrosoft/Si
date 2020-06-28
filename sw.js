a671337881d200b4b44e3b21d542e32b907d5205
sinrosoft <67509752+sinrosoft@users.noreply.github.com>
2020-06-28 14:06:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,38 @@
-'use strict';
-
-importScripts('sw-toolbox.js');
-
-toolbox.precache(["index.html","style/style.css"]);
-
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-
-toolbox.router.get('/*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/assets/js/ui.js',
+  '/assets/css/main.css',
+  '/assets/images/background-home.jpg',
+  'https://fonts.googleapis.com/css?family=Lato:300,400,700',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// When we change the name we could have multiple cache, to avoid that we need to delet the old cache, so with this function we check the key that is our cache naming, if it is different from the actual naming we delete it, in this way we will always have only the last updated cache.
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
